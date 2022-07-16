93a7214b2e784bbdbca1f736bcec1f1eae1f583c
cccaaannn <can.kurt.aa@gmail.com>
2022-07-16 18:18:40 +0300

    cors

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"zup-message-service/database"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -25,12 +26,17 @@ func main() {
 	RegisterMessageRoutes(router)
 
 	// Start the server
+	handler := cors.AllowAll().Handler(router)
 	log.Println(fmt.Sprintf("Starting Server on port %s", config.AppConfig.Port))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.AppConfig.Port), router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.AppConfig.Port), handler))
 }
 
 func RegisterMessageRoutes(router *mux.Router) {
 	router.HandleFunc("/api/v1/messages", controller.GetMessages).Methods("GET")
 	// router.HandleFunc("/api/messages/{id}", controller.GetMessageById).Methods("GET")
 	router.HandleFunc("/api/v1/messages", controller.CreateMessage).Methods("POST")
+	router.HandleFunc("/api/v1/messages/SetMessageAsRead/{id}", controller.SetMessageAsRead).Methods("PUT")
+	router.HandleFunc("/api/v1/messages/GetAllMessages/{fromId}/{toId}", controller.GetAllMessages).Methods("GET")
+
+	router.HandleFunc("/api/v1/ws/{after}/{user}", controller.WS)
 }
