3565ff3afd434c6f1db8253023e8300aad842ccf
cccaaannn <can.kurt.aa@gmail.com>
2022-07-30 22:43:07 +0300

    UserOnlineStatus service

---
+++ services/user_online_status_service.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"time"
+	"zup-message-service/database"
+	"zup-message-service/models"
+)
+
+func UpdateLastUserLogin(userId int, status int) bool {
+	var userOnlineStatus models.UserOnlineStatus
+	userOnlineStatus.UserId = userId
+	userOnlineStatus.IsOnline = status
+	userOnlineStatus.LastOnline = time.Now()
+
+	database.Connection.Save(&userOnlineStatus)
+	return true
+}
+
+func GetLastUserLogin(userId int) models.UserOnlineStatus {
+	var userOnlineStatus models.UserOnlineStatus
+	database.Connection.First(&userOnlineStatus, userId)
+	return userOnlineStatus
+}
