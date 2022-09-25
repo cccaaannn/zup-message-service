9c3c059df8be6156229f12675629515cc8bf23ac
cccaaannn <can.kurt.aa@gmail.com>
2022-09-25 17:38:21 +0300

    rest client changed

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module zup-message-service
 go 1.19
 
 require (
+	github.com/go-resty/resty/v2 v2.7.0
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/rs/cors v1.8.2
@@ -35,6 +36,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
--- services/user_service.go
+++ services/user_service.go
@@ -3,13 +3,14 @@ package services
 import (
 	"fmt"
 	"log"
+    "encoding/json"
 
-	"zup-message-service/utils"
 	"zup-message-service/configs"
 	"zup-message-service/data/dtos"
+	"github.com/go-resty/resty/v2"
 )
 
-
+var client = resty.New()
 var userServiceApiPath = "api/v1"
 
 func getUserServiceBasePath() string {
@@ -18,21 +19,56 @@ func getUserServiceBasePath() string {
 
 func IsAuthorized(userToken string) dtos.DataResult[dtos.TokenPayload] {
 	requestURL := fmt.Sprintf(getUserServiceBasePath() + "/authorization/%s", userToken)
-	log.Println(requestURL)
+	log.Printf("Requesting user service, URL: %s\n", requestURL)
+	res := dtos.DataResult[dtos.TokenPayload]{Status:false, Message: "Error", Data: nil}
+	
+	resp, err := client.R().EnableTrace().Get(requestURL)
 
-	return utils.GetApiDataResult[dtos.TokenPayload](requestURL)
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = json.Unmarshal(resp.Body(), &res)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+	log.Printf("Response from user service: %+v\n\n", res)
+	return res
 }
 
 func SetUserOnlineStatus(userId uint64, newStatus string, accessToken string) dtos.Result {
 	requestURL := fmt.Sprintf(getUserServiceBasePath() + "/user/%d/online-status/%s", userId, newStatus)
-	log.Println(requestURL)
+	log.Printf("Requesting user service, URL: %s\n", requestURL)
+	res := dtos.Result{Status:false, Message: "Error"}
 
-	return utils.PutApiResult(requestURL, accessToken)
+	resp, err := client.R().
+		SetAuthToken(accessToken).
+      	Put(requestURL)
+
+	err = json.Unmarshal(resp.Body(), &res)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	log.Printf("Response from user service: %+v\n\n", res)
+	return res
 }
 
 func GetUserOnlineStatus(userId uint64, accessToken string) dtos.DataResult[dtos.UserOnlineStatus] {
 	requestURL := fmt.Sprintf(getUserServiceBasePath() + "/user/%d/online-status", userId)
-	log.Println(requestURL)
+	log.Printf("Requesting user service, URL: %s\n", requestURL)
+	res := dtos.DataResult[dtos.UserOnlineStatus]{Status:false, Message: "Error", Data: nil}
 
-	return utils.GetApiDataResultWithToken[dtos.UserOnlineStatus](requestURL, accessToken)
+	resp, err := client.R().
+		SetAuthToken(accessToken).
+      	Get(requestURL)
+
+	err = json.Unmarshal(resp.Body(), &res)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	log.Printf("Response from user service: %+v\n\n", res)
+	return res
 }
