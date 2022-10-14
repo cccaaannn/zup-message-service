b12ae100be4917ae9ed93a916fef97797b128cd4
cccaaannn <can.kurt.aa@gmail.com>
2022-10-14 22:30:47 +0300

    system config now overrides config file

--- configs/app_config.go
+++ configs/app_config.go
@@ -2,6 +2,8 @@ package configs
 
 import (
 	"log"
+	"os"
+	"reflect"
 
 	"github.com/spf13/viper"
 )
@@ -13,28 +15,59 @@ type Config struct {
 	PostgresqlConnectionString string `mapstructure:"POSTGRESQL_CONNECTION_STRING"`
 	RabbitmqConnectionString   string `mapstructure:"RABBITMQ_CONNECTION_STRING"`
 
-	UserServiceUrl  string `mapstructure:"USER_SERVICE_URL"`
-	UserServicePort string `mapstructure:"USER_SERVICE_PORT"`
+	ApiPathPrefix string `mapstructure:"API_PATH_PREFIX"`
+
+	UserServiceUrl      string `mapstructure:"USER_SERVICE_URL"`
+	UserServiceBasePath string `mapstructure:"USER_SERVICE_BASE_PATH"`
 }
 
 func LoadConfig() {
-	log.Println("Loading config...")
+	log.Println("Loading system env variables...")
 
+	/*
+	 * Since I couldn't find any better lib alternative, before reading env values from file read them from sys env to override file values
+	 */
+	var appConfigTemp Config
+	appConfigTempFields := reflect.ValueOf(appConfigTemp)
+	for i := 0; i < appConfigTempFields.Type().NumField(); i++ {
+		envName := appConfigTempFields.Type().Field(i).Tag.Get("mapstructure")
+		envValue := os.Getenv(envName)
+		log.Printf("%s=%s\n", envName, envValue)
+		reflect.ValueOf(&appConfigTemp).Elem().Field(i).SetString(envValue)
+	}
+
+	log.Printf("Loading file env variables...\n\n")
 	viper.AddConfigPath(".")
 	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
-
-	viper.AutomaticEnv()
-
 	err := viper.ReadInConfig()
-
 	if err != nil {
-		log.Fatalf("Error while reading config file %s", err)
+		log.Printf("Error while reading config file %s\n", err)
 	}
 
 	err = viper.Unmarshal(&AppConfig)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+	}
+
+	/*
+	* Replace file env variables with system.
+	 */
+	log.Println("Final env variables. (System overrides)")
+	for i := 0; i < appConfigTempFields.Type().NumField(); i++ {
+		envName := appConfigTempFields.Type().Field(i).Tag.Get("mapstructure")
+		envValue := reflect.ValueOf(appConfigTemp).Field(i).String()
+		if envValue != "" {
+			reflect.ValueOf(AppConfig).Elem().Field(i).SetString(envValue)
+		} else {
+			envValue = reflect.ValueOf(*AppConfig).Field(i).String()
+		}
+
+		if envValue == "" {
+			log.Fatalf("Environment value %s is not defined.", envName)
+		}
+
+		log.Printf("%s=%s\n", envName, envValue)
 	}
 
 }
